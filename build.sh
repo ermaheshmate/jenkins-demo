e0e0b2f7077d0f048e94adfaa0a2aad6c40c4059
Mahesh Mate <ermaheshmate@gmail.com>
2026-07-16 10:52:51 +0000

    Added built script

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "===================="
+echo "Starting Build..."
+echo "===================="
+
+echo "Current User:"
+whoami
+
+echo ""
+
+echo "Current Directory:"
+pwd
+
+echo ""
+
+echo "Listing Files:"
+ls -la
+
+echo ""
+
+echo "Reading index.txt"
+
+cat index.txt
+
+echo ""
+
+echo "Build Successful"
